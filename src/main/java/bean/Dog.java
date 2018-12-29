fda41b8843630e841a7cadefaf55f85ef125bb23
jason <zengjin_liu@aliyun.com>
2018-12-29 09:02:07 +0800

    idea 整合测试GitHub

---
+++ src/main/java/bean/Dog.java
@@ -0,0 +1,23 @@
+package bean;
+
+import java.io.Serializable;
+
+/**
+ * @ClassName Dog
+ * @Description TODO
+ * @Author Administrator
+ * @Date 2018/12/29 9:00
+ */
+public class Dog implements Serializable {
+
+	private static final long serialVersionUID = 2536570914214785960L;
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
